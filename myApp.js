1173b62a991ee256dcc7f82b476382057f562e64
mdfakih <mf9049@gmail.com>
2023-05-19 12:31:33 +0530

    send html file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express')
+let path = require('path')
 let app = express()
 
 console.log('Hello World!')
 
+// app.get('/', function (req, res) {
+//     res.send("Hello Express")
+// })
+
 app.get('/', function (req, res) {
-    res.send("Hello Express")
+    res.sendFile(path.join(__dirname, './views', 'index.html'))
+    // res.sendFile('index.html', { root: path.join(__dirname, './views') })
 })
 
 
