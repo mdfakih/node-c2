02a0146fa437e101bff6f8a1f87a4e5a4acc52d3
mdfakih <mf9049@gmail.com>
2023-05-22 11:08:42 +0530

    use env variable

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 let express = require('express')
+require('dotenv').config()
 let path = require('path')
 let app = express()
 
@@ -16,7 +17,11 @@ app.get('/', function (req, res) {
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/json', function (req, res) {
-    res.json({ "message": "Hello json" })
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ "message": "HELLO JSON" })
+    } else {
+        res.json({ "message": "Hello json" })
+    }
 })
 
 
