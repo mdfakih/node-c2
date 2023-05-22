d80e4963b0de46f274f9e9a8f16929afa2e0b1d5
mdfakih <mf9049@gmail.com>
2023-05-22 16:46:32 +0530

    param input

--- myApp.js
+++ myApp.js
@@ -4,38 +4,42 @@ let path = require('path')
 
 console.log('Hello World!')
 
+app.get('/:word/echo', function (req, res) {
+    res.send({ echo: req.params.word })
+})
+
 // app.get('/', function (req, res) {
 //     res.send("Hello Express")
 // })
 
-app.use(function (req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip}`)
-    // console.log(req.method + " " + req.path + " - " + req.ip);
-    next()
-})
+// app.use(function (req, res, next) {
+//     console.log(`${req.method} ${req.path} - ${req.ip}`)
+//     // console.log(req.method + " " + req.path + " - " + req.ip);
+//     next()
+// })
 
-app.get('/now', function (req, res, next) {
-    req.time = new Date().toString()
-    next()
-}, function (req, res) {
-    res.send({ time: req.time })
-})
+// app.get('/now', function (req, res, next) {
+//     req.time = new Date().toString()
+//     next()
+// }, function (req, res) {
+//     res.send({ time: req.time })
+// })
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './views', 'index.html'))
-    // res.sendFile('index.html', { root: path.join(__dirname, './views') })
-})
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, './views', 'index.html'))
+//     // res.sendFile('index.html', { root: path.join(__dirname, './views') })
+// })
 
-app.use('/public', express.static(__dirname + '/public'))
+// app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/json', function (req, res) {
-    let string = 'Hello json'
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json({ "message": string.toUpperCase() })
-    } else {
-        res.json({ "message": string })
-    }
-})
+// app.get('/json', function (req, res) {
+//     let string = 'Hello json'
+//     if (process.env.MESSAGE_STYLE === 'uppercase') {
+//         res.json({ "message": string.toUpperCase() })
+//     } else {
+//         res.json({ "message": string })
+//     }
+// })
 
 
 
